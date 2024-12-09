c9fc3fc8bf9aef16c732b0e7e7ab89b309085550
Alexis <90972106+alexishadjian@users.noreply.github.com>
2024-12-09 15:31:33 +0100

    feat: add style starter, setup gulp

--- wp-content/wflogs/config.php
+++ wp-content/wflogs/config.php
@@ -3,4 +3,4 @@
 This file is used by the Wordfence Web Application Firewall. Read 
 more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
 ******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1734345461;s:7:"authKey";s:64:";bE?a9{2QlUe*pnQ&&~]zdzV~Igb;u,{9`o@1y..s%;gSQLgPb ,)l*u-J~M:v(m";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:2221;}
+a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1734345461;s:7:"authKey";s:64:";bE?a9{2QlUe*pnQ&&~]zdzV~Igb;u,{9`o@1y..s%;gSQLgPb ,)l*u-J~M:v(m";s:7:"version";s:5:"1.1.0";s:11:"wafDisabled";b:1;s:13:"attackDataKey";i:2221;}
