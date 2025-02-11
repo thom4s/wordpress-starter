f8552c023afa344d59e363998912a23b8b4ee14f
Alexis <90972106+alexishadjian@users.noreply.github.com>
2025-02-11 11:11:03 +0100

    feat: add footer content

--- wp-content/themes/custom-theme/footer.php
+++ wp-content/themes/custom-theme/footer.php
@@ -12,14 +12,31 @@
 ?>
 
 	<footer id="colophon" class="site-footer">
-		<p id="copyright">Copyright © <?= date("Y"); ?> <?= bloginfo('name'); ?></p>
+		<div class="wrapper">
+			<div class="flex -space mb-l">
+				<?php
+				$logo = get_field('logo_du_site', 'option'); 
+				if( $logo ): ?>
+					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" class="site-logo">
+						<img src="<?php echo esc_url( $logo ); ?>" alt="Logo">
+					</a>
+				<?php endif; ?>
 
-		<nav id="footer-navigation" role="navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'footer-menu', 'container' => false ) ); ?>
-		</nav>
+				<nav id="footer-navigation" role="navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'header-menu-fr', 'menu_id' => 'footer-menu', 'container' => false ) ); ?>
+				</nav>
 
-		<div id="wp-footer">
-			<?php wp_footer(); ?>
+				<nav id="footer-navigation" role="navigation">
+					<?php wp_nav_menu( array( 'theme_location' => 'footer-menu-fr', 'menu_id' => 'footer-menu', 'container' => false ) ); ?>
+				</nav>
+			</div>
+
+			<p id="copyright" class="txt-center">Copyright © <?= date("Y"); ?> <?= bloginfo('name'); ?></p>
+
+
+			<div id="wp-footer">
+				<?php wp_footer(); ?>
+			</div>
 		</div>
 	</footer><!-- #colophon -->
 </div><!-- #page -->
